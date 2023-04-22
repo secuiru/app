6f6e8ef4534984afe4fa62c7e73353aea1e0d584
luolamies96 <tommijouppila@hotmail.com>
2023-04-22 11:22:51 +0300

    vipat viimeestelyt

--- ezfood/lib/favourites.dart
+++ ezfood/lib/favourites.dart
@@ -15,10 +15,11 @@ final List<int> colorCodes = <int>[600, 500, 100];
 
 Widget build(BuildContext context) {
   return ListView.separated(
-    padding: const EdgeInsets.all(8),
+    
     itemCount: entries.length,
     itemBuilder: (BuildContext context, int index) {
       return Container(
+        
         height: 50,
         color: Colors.amber[colorCodes[index]],
         child: Center(child: Text('Entry ${entries[index]}')),
